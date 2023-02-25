9aea53aef71b8f633faee2c11c5f32336a198010
Xevion <ryanchwalters@gmail.com>
2023-02-24 19:40:38 -0600

    Add atomic health check API route

---
+++ src/pages/api/health.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { env } from "@/env/server.mjs";
+
+type ResponseData = {
+  now: number;
+  status: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({
+    now: new Date().getTime(),
+    status: req.query.key == env.API_KEY ? "Authorized" : "Unauthorized",
+  });
+}
