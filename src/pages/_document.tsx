d25bb407e35e4b6028933eabcdfcbd8464fc44a4
Xevion <xevion@xevion.dev>
2023-09-05 12:38:10 -0500

    Add default title to all pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html className="h-full bg-gray-50">
-            <Head/>
-            <body className="h-full">
-            <Main/>
-            <NextScript/>
-            </body>
-        </Html>
-    )
+	return (
+		<Html className="h-full bg-gray-50">
+			<Head>
+				<title>bus-reminder</title>
+			</Head>
+			<body className="h-full">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
