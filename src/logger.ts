48d5f0b3f974beefc5c6ea4575e9712b788d1e75
Xevion <xevion@xevion.dev>
2023-11-15 17:10:18 -0600

    Disable colorized logging, fix production check

--- src/logger.ts
+++ src/logger.ts
@@ -23,7 +23,7 @@ const logger: Logger = createLogger({
 		}),
 		new transports.Console({
 			format: format.combine(
-				format.colorize(),
+				env.NODE_ENV == 'production' ? format.json() : format.colorize(),
 				format.printf(
 					({ level, message, timestamp, stack }) =>
 						`${timestamp} ${level}: ${stack || message}`
